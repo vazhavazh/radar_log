8687de4097d06b43432ed1c241bd4e7a5592e14a
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2025-07-26 06:01:37 +0300

    2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { formSubmit } from "./formSubmit.js";
 import { switchTabs } from "./switchTabs.js";
-import { radarBlindSectorDiagram } from "./radarBlindSector.js";
-radarBlindSectorDiagram();
+import { radarBlindSectorDiagram1 } from "./radarBlindSector.js";
+import { radarBlindSectorDiagram2 } from "./radarBlindSector.js";
+radarBlindSectorDiagram1();
+radarBlindSectorDiagram2();
+
 formSubmit();
 switchTabs();
